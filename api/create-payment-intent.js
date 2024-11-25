27630f152d49115b363d35b391a5814dc3f37c3b
GivingHeartsInternational <144336851+GivingHeartsInternational@users.noreply.github.com>
2024-11-25 22:38:32 +0300

    Update test create-payment-intent.js

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,24 +1,3 @@
-// api/create-payment-intent.js
-
-const Stripe = require('stripe');
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY); // Секретный ключ Stripe
-
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
-
-  const { amount } = req.body;
-
-  try {
-    // Создаем платежное намерение
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount, // Сумма в центах
-      currency: 'usd',
-    });
-
-    res.status(200).json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+export default function handler(req, res) {
+  res.status(200).json({ message: "API is working!" });
 }
